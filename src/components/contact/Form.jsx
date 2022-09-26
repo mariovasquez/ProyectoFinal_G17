d0c594873b1b3e3749d88851111d2f88d9969289
capolancoz <capolancoz@gmail.com>
2022-09-26 14:14:27 -0500

    Update contactPage errorPage

---
+++ src/components/contact/Form.jsx
@@ -0,0 +1,29 @@
+const Form = () => {
+  return (
+    <div className="contact__form">
+    <h2 className="contact__title">Contáctanos</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida urna erat.</p>         
+     <form className="contact__form">
+        <label htmlFor="nombre">
+            <input className="contact__input" type="text" name="fname" placeholder="Nombre" id="nombre" required/>
+        </label>
+        <label htmlFor="apellido">
+            <input className="contact__input" type="text" name="lname" placeholder="Apellido" id="apellido" required/>
+        </label>
+        <label htmlFor="correo">
+            <input className="contact__input" type="text" name="lname" placeholder="ejemplo@gmail.com" id="correo" required/>
+        </label>
+        <label htmlFor="telefono">
+            <input className="contact__input" type="tel" name="tel" placeholder="999666333" id="telefono" required/>
+        </label>
+        <label htmlFor="asunto">
+            <textarea className="contact__input" name="message" rows="3" placeholder="Asunto" id="asunto" required/>
+        </label>
+        <button className="contact__button" type="submit">Enviar</button>
+      </form>
+      
+    </div>
+  );
+}
+ 
+export default Form;
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,21 @@
+import Footer from '../components/sections/Footer';
+import ContactImg from '../assets/img/hero.jpg'
+import Form from '../components/contact/Form';
+
 const ContactPage = () => {
     return (
         <>
-            <h2>Contacto</h2>
+        <article className="contact">
+            <div className="container">
+                <div className="contact__wrapper">
+                <Form/>
+                    <div>
+                        <img src={ContactImg} alt="Vao_contacto" height="570" width="500" className='contact__img'/>
+                    </div> 
+                </div>
+            </div>
+        </article>
+        <Footer/>
         </>
     );
 }
