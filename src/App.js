58444b0e5737eb29311a281f808761c1ac1a9be8
capolancoz <capolancoz@gmail.com>
2022-10-14 10:22:41 -0500

    Add Redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import Header from './components/sections/Header';
 import './styles/css/index.css';
 import './styles/css/library.css';
 import IndexPage from './pages/IndexPage';
-import Footer from './components/sections/Footer';
 import { BrowserRouter, Routes, Link, Route } from "react-router-dom";
 import AboutPage from './pages/AboutPage';
 import Layout from './layout/Layout';
@@ -10,11 +8,12 @@ import ContactPage from './pages/ContactPage';
 import NotFoundPage from './pages/NotFoundPage';
 import StorePage from './pages/StorePage';
 import ItemPage from './pages/ItemPage';
+import { store } from './redux/store';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter store={store}>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<IndexPage/>}></Route>
