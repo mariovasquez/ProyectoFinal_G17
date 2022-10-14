bc2d3672627c1202bcd7ce8b280f68497b6b7a39
mariovasquez <N00156666@upn.pe>
2022-10-14 18:34:35 -0500

    get data store page

--- src/pages/StorePage.jsx
+++ src/pages/StorePage.jsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
 const StorePage = () => {
+    // Fetch JSON
+    const [albumsData, setAlbumsData] = useState([]);
+
+    useEffect(() => {
+        getAlbumsData("https://raw.githubusercontent.com/mariovasquez/ProyectoFinal_G17/develop/src/json/albums.json");
+    }, [])
+
+    const getAlbumsData = async (url) => {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            const albumsArray = [];
+            // data.map((element, index) => {
+            //         albumsArray.push(element)
+            // })
+            setAlbumsData(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    console.log('GAAAA'+albumsData);
     return (
-        <h2>Store</h2>
+        <section className="section">
+            <div className="container__store">
+            {
+                        albumsData.map((element, index) => (
+                                <div className="section__featured-album-container" data-key={index}>
+                                    <Link to={`/store/${element.id}`} className="link">
+                                        <div className="container__album">
+                                            <img src={element.image_url} alt="" />
+                                            {console.log(element)}
+                                        </div>
+                                    </Link>
+                                </div>
+                        ))
+                    }
+            </div>
+        </section>
     );
 }
  
